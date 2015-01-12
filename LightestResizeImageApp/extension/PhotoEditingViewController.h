17dca89ba5585b026a8b7370f9b1f49bb96b12e5
Hai Hw <nguyenhuuhiephai@gmail.com>
2015-01-12 17:06:41 +0800

    info

---
+++ LightestResizeImageApp/extension/PhotoEditingViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoEditingViewController.h
+//  extension
+//
+//  Created by Hai Hw on 2/1/15.
+//  Copyright (c) 2015 Hai Hw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoEditingViewController : UIViewController
+
+@end
